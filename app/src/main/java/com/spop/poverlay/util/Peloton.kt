00571b99c33182a15c6a837a9a3295f4df85de14
Anthony Doud <anthonydoud97@gmail.com>
2026-01-21 09:33:33 -0600

    Increased Bike + models support

--- app/src/main/java/com/spop/poverlay/util/Peloton.kt
+++ app/src/main/java/com/spop/poverlay/util/Peloton.kt
@@ -9,21 +9,20 @@ private const val PelotonBrand = "Peloton"
 val IsRunningOnPeloton = Build.BRAND == PelotonBrand
 
 /**
- * I'm not sure if this is the best way to determine if the device is a Peloton Bike+.
- * I'm assuming that the model name is the same for all Peloton Bike+ devices based on what I'm able
- * to find online.
+ * All Peloton bikes start with model "PLTN-T". Treadmills start with "PLTN-TR", so this might also
+ * apply to them, but it will be interesting if anything works on them here.
  */
-val IsBikePlus = Build.MODEL.startsWith("PLTN-TTR01")
+val IsBikePlus = Build.MODEL.startsWith("PLTN-T")
 
 fun calculateSpeedFromPelotonV1Power(power: Float) =
-    if (power < 0.1f) {
-        0f
-    } else {
-        //https://ihaque.org/posts/2020/12/25/pelomon-part-ib-computing-speed/
-        val pwrSqrt = sqrt(power)
-        if (power < 26f) {
-            0.057f - (0.172f * pwrSqrt) + (0.759f * pwrSqrt.pow(2)) - (0.079f * pwrSqrt.pow(3))
+        if (power < 0.1f) {
+            0f
         } else {
-            -1.635f + (2.325f * pwrSqrt) - (0.064f * pwrSqrt.pow(2)) + (0.001f * pwrSqrt.pow(3))
+            // https://ihaque.org/posts/2020/12/25/pelomon-part-ib-computing-speed/
+            val pwrSqrt = sqrt(power)
+            if (power < 26f) {
+                0.057f - (0.172f * pwrSqrt) + (0.759f * pwrSqrt.pow(2)) - (0.079f * pwrSqrt.pow(3))
+            } else {
+                -1.635f + (2.325f * pwrSqrt) - (0.064f * pwrSqrt.pow(2)) + (0.001f * pwrSqrt.pow(3))
+            }
         }
-    }
